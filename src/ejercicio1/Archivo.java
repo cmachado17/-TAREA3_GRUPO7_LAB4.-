e74c91ad01be1836f42adf36aab860b1db0b05b3
SebaGines <sebastian.gines@gmail.com>
2023-04-16 12:42:42 -0300

    Fix Archivo class

--- src/ejercicio1/Archivo.java
+++ src/ejercicio1/Archivo.java
@@ -1,5 +1,81 @@
 package ejercicio1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Archivo {
 
+	//Si en la variable ruta recibe solo un nombre de un archivo, entonces crea ese archivo dentro del proyecto
+	//Si en ruta recibe una ruta absoluta, entonces lo crea en esa ruta
+	
+	private String ruta;
+
+	public boolean existe()
+	{
+		File archivo = new File(ruta); 
+		if(archivo.exists())
+		      return true;
+		return false;
+	}
+	
+	public boolean creaArchivo()
+	{
+		FileWriter escritura;
+		try {
+			escritura = new FileWriter(ruta, true);
+			escritura.write("");
+			escritura.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+			
+	}
+	
+	
+	public void escribe_lineas(String frase) {
+		try 
+		{	
+			FileWriter entrada = new FileWriter(ruta, true);
+			BufferedWriter miBuffer = new BufferedWriter(entrada);
+			miBuffer.write(frase);
+			miBuffer.close();
+			entrada.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void lee_lineas() {
+		FileReader entrada;
+		try {
+			entrada = new FileReader(ruta);
+			BufferedReader miBuffer = new BufferedReader(entrada);
+			
+		   String linea = "";
+			while (linea != null) {
+				System.out.println(linea);
+				linea = miBuffer.readLine();
+			}
+			miBuffer.close();
+			entrada.close();
+
+		} catch (IOException e) {
+			System.out.println("No se encontro el archivo");
+		}
+	}
+
+	public String getRuta() {
+		return ruta;
+	}
+
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
 }
